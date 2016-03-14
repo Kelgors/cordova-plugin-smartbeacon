d819eb32677b0f2dad55c572ca11f0f99797b1ce
Kelgors <matthieuboheas@gmail.com>
2016-03-14 12:17:22 +0100

    add callbacks for {start,stop}Monitoring

--- src/android/BeaconManager.java
+++ src/android/BeaconManager.java
@@ -44,9 +44,11 @@ public class BeaconManager extends CordovaPlugin {
             if ("startMonitoring".equals(action)) {
                 _listener.clear();
                 _manager.startMonitoringAllBeaconRegions();
+                callbackContext.sendPluginResult(new PluginResult(PluginResult.OK));
                 return true;
             } else if ("stopMonitoring".equals(action)) {
                 _manager.stopMonitoringAllBeaconRegions();
+                callbackContext.sendPluginResult(new PluginResult(PluginResult.OK));
                 return true;
             } else if ("getDetectedBeacons".equals(action)) {
                 commandGetDetectedBeacons(callbackContext);
